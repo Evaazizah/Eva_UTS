a2e60f790c9188983f7f5876c6656716001f58fc
Eva_Azizah <effatulazizah04@gmail.com>
2025-04-25 20:15:23 +0700

    index

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,31 @@
-import { Text, View } from "react-native";
+import { useEffect } from "react";
+import { View, Image, StyleSheet } from "react-native";
 
-export default function Index() {
+
+export default function SplashScreen() {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Image source={require('../assets/images/APOTEK.jpeg')} style={styles.logo} resizeMode="contain"/>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  logo: {
+    width: 200,
+    height: 200,
+  },
+});
